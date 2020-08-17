dfd6bb1d692ab06182ef5072fa6f733c34c34a42
tmm2176 <tmmcpher@ucsc.edu>
2020-08-16 22:48:14 -0700

    adding nav component skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TrialNavComponent } from './trial-nav/trial-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrialNavComponent
   ],
   imports: [
     BrowserModule,

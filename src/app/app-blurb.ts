9c71d24cd139574cbf2ae05a89c334ca9c8d7f30
tmm2176 <tmmcpher@ucsc.edu>
2020-08-19 12:27:32 -0700

    updating images and bindings

--- src/app/app-blurb.ts
+++ src/app/app-blurb.ts
@@ -2,4 +2,6 @@ export interface AppBlurb {
   id: number;
   title: string;
   description: string;
+  imgUrl: string;
+  imgAlt: string;
 }
--- src/app/blurbs-directory.ts
+++ src/app/blurbs-directory.ts
@@ -7,6 +7,8 @@ export const BLURBS: AppBlurb[] = [
       " world for the better. Nothing is too crazy to imagine. One of the most influential " +
       "forces of socially-grounded tech in my life is the Tech4Good Lab, where I’m a Fellow" +
       " and work as a UX Reseracher and Designer.",
+    imgUrl: '/../assets/img/computer.png',
+    imgAlt: 'computer',
   },
   { id: 3,
     title: "Slow Fashion & Sustainability ",
@@ -15,6 +17,8 @@ export const BLURBS: AppBlurb[] = [
       " fashion industry's impact on our climate and social systems. To push against some " +
       "of its negative impacts, I started an online-thrift shop, Circular Goods, as a passion" +
       " project.",
+    imgUrl: '/../assets/img/recycle.png',
+    imgAlt: 'recycle',
   },
   { id: 2,
     title: "Wicked Problems & User-Centered Design",
@@ -22,16 +26,22 @@ export const BLURBS: AppBlurb[] = [
       "high-quality education? What do communities look like that centers systems of care" +
       " rather than policing? These are some of many wicked problems I could get lost in." +
       " I don’t have the answers yet, but chatting about it together is a great start. ",
+    imgUrl: '/../assets/img/wtf.png',
+    imgAlt: '?!',
   },
   { id: 4,
     title: "Hawai‘i",
     description: "Hawai‘i is home for me, and love talking about this special place. " +
       "Bonus points if you mention Sovereignty and the Hawaiian Kingdom.",
+    imgUrl: '/../assets/img/beach.png',
+    imgAlt: 'beach',
   },
   { id: 5,
     title: "Dogs, Coffee & Croissants",
     description: "Preferably in that order. " +
       "When I’m not working or learning, I’m going on a long, romantic walks on the beach" +
       " with my dog Dolly, ideally sipping Cat & Cloud Coffee with a croissant in hand.",
+    imgUrl: '/../assets/img/dog.png',
+    imgAlt: 'dog',
   },
 ];

af3b06c6e969609b14ac9afca3974a4c1d65b49f
tmm2176 <tmmcpher@ucsc.edu>
2020-08-19 13:01:53 -0700

    updating work card imgs

--- src/app/cards-directory.ts
+++ src/app/cards-directory.ts
@@ -9,17 +9,17 @@ export const CARDS: WorkCard[] = [
   { id: 2,
     title: 'USER RESEARCH',
     description: 'UX Researcher, Writer',
-    imgUrl: 'url(/../assets/img/card-2.png)',
+    imgUrl: 'url(./assets/img/card-2.png)',
   },
   { id: 3,
     title: 'CRITICAL DESIGN',
     description: 'Researcher, Visual Designer',
-    imgUrl: 'url(/../assets/img/card-3.png)'
+    imgUrl: 'url(./assets/img/card-3.png)'
   },
   { id: 4,
     title: 'PLAYGROUND',
     description: 'Side Projects',
-    imgUrl: 'url(/../assets/img/card-4.png)',
+    imgUrl: 'url(./assets/img/card-4.png)',
   },
 
 ];

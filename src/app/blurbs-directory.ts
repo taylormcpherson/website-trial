26300084cd3c27d86da8dfe9662fec10cf99bd98
tmm2176 <tmmcpher@ucsc.edu>
2020-08-21 21:48:45 -0700

    adjustments to about blurbs

--- src/app/blurbs-directory.ts
+++ src/app/blurbs-directory.ts
@@ -3,10 +3,10 @@ import {AppBlurb} from "./app-blurb";
 export const BLURBS: AppBlurb[] = [
   { id: 1,
     title: "Tech for Social Good",
-    description: "I get giddy thinking about radical ways technologies can transform our" +
-      " world for the better. Nothing is too crazy to imagine. One of the most influential " +
+    description: "I get really excited thinking about radical ways technologies can transform our" +
+      " world for the better. Nothing is too crazy. One of the most influential " +
       "forces of socially-grounded tech in my life is the Tech4Good Lab, where I’m a Fellow" +
-      " and work as a UX Reseracher and Designer.",
+      " and work as a UX Researcher and Designer.",
     imgUrl: '💻',
     imgAlt: 'computer',
   },
@@ -15,23 +15,23 @@ export const BLURBS: AppBlurb[] = [
     description: "When I was little I wanted to be a fashion designer." +
       " My passions have aligned elsewhere since then, but I'm now fascinated by the" +
       " fashion industry's impact on our climate and social systems. To push against some " +
-      "of its negative impacts, I started an online-thrift shop, Circular Goods, as a passion" +
+      "of its negative impacts I recently started an online-thrift shop as a passion" +
       " project.",
     imgUrl: '♻️',
     imgAlt: 'recycle',
   },
   { id: 2,
     title: "Wicked Problems in Design",
-    description: "How do we design a world that allows every child access to free, " +
-      "high-quality education? What do communities look like that centers systems of care" +
-      " rather than policing? These are some of many wicked problems I could get lost in." +
+    description: "How do we design a world that allows every child access to " +
+      "high-quality education? What do communities look like that center systems of care" +
+      " rather than policing? These are some of many wicked problems I could think about for days on end." +
       " I don’t have the answers yet, but chatting about it together is a great start. ",
     imgUrl: '⁉️️',
     imgAlt: '?!',
   },
   { id: 4,
     title: "Hawai‘i",
-    description: "Hawai‘i is home for me, and love talking about this special place. " +
+    description: "Hawai‘i is home for me, and I love talking about this special place. " +
       "Bonus points if you mention Sovereignty and the Hawaiian Kingdom.",
     imgUrl: '🏝️',
     imgAlt: 'beach',
@@ -39,7 +39,7 @@ export const BLURBS: AppBlurb[] = [
   { id: 5,
     title: "Dogs, Coffee & Croissants",
     description: "Preferably in that order. " +
-      "When I’m not working or learning, I’m going on a long, romantic walks on the beach" +
+      "When I’m not learning or working, I’m going on a long, romantic walks on the beach" +
       " with my dog Dolly, ideally sipping Cat & Cloud Coffee with a croissant in hand.",
     imgUrl: '🐶',
     imgAlt: 'dog',
